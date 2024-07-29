51a1bf6af2de10f6213ae155aeda1ae1f29006e6
Hardik-G1 <hardik4869@gmail.com>
2024-07-29 20:39:03 +0530

    Footer Changed

--- bingo/src/Components/Footer.tsx
+++ bingo/src/Components/Footer.tsx
@@ -1,7 +1,7 @@
 import "./css/Footer.css";
 function Footer(){
     return (
-    <footer className="App-footer">Footer</footer>
+    <footer className="App-footer">....Design in Progress</footer>
     );
 }
 export default Footer;
